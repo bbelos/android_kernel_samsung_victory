ee86a2c8661bb74014cbeb4325460643e7909136
Brian Beloshapka <bbelos@hotmail.com>
2012-05-23 19:38:48 -0500

    Revert "Define safe initial min and max speeds on boot, thanks to nubecoder."
    Part 3: Preparation for cleaner OC code.
    
    This reverts commit 847cfac9eaa03297c7f485a73694ce92e8dd7b3f.

--- Kernel/arch/arm/mach-s5pv210/cpu-freq.c
+++ Kernel/arch/arm/mach-s5pv210/cpu-freq.c
@@ -767,11 +767,7 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
                 g_dvfslockval[i] = MAX_PERF_LEVEL;
 #endif
 
-	cpufreq_frequency_table_cpuinfo(policy, freq_table);
-	/* set default min and max policies to safe speeds */
-	policy->max = 1000000;
-	policy->min = 100000;
-	return 0;
+	return cpufreq_frequency_table_cpuinfo(policy, freq_table);
 }
 
 static int s5pv210_cpufreq_notifier_event(struct notifier_block *this,
